0892a4a9724618813f5418e5fc01b9b2b02f41c6
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-25 20:01:30 +0530

    Added accounts and branches tables

--- SCHEMA/create_tables.sql
+++ SCHEMA/create_tables.sql
@@ -40,3 +40,53 @@ CREATE TABLE accounts (
         FOREIGN KEY (customer_id)
         REFERENCES customers(customer_id)
 );
+
+CREATE TABLE branches (
+    branch_id INT AUTO_INCREMENT PRIMARY KEY,
+    branch_code CHAR(20) UNIQUE NOT NULL,
+    branch_manager_id INT NOT NULL UNIQUE,
+
+    building_no VARCHAR(20),
+    street VARCHAR(50),
+    area VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    pincode CHAR(10) NOT NULL,
+
+    ifsc_code CHAR(11) UNIQUE NOT NULL,
+    contact_number VARCHAR(15),
+    branch_email VARCHAR(100),
+
+    branch_status VARCHAR(20) DEFAULT 'Active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE branches
+ADD COLUMN ifsc_code_branch CHAR(11) UNIQUE NOT NULL;
+
+ALTER TABLE branches
+ADD COLUMN contact_number_branch VARCHAR(15);
+
+ALTER TABLE branches
+ADD COLUMN branch_email VARCHAR(100) UNIQUE;
+
+ALTER TABLE branches
+ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+select * from branches ;
+
+alter table branches drop column contact_number_branch ;
+alter table branches drop column ifsc_code_branch ;
+
+
+ALTER TABLE accounts
+DROP COLUMN branch_code;
+
+ALTER TABLE accounts
+ADD COLUMN branch_id INT NOT NULL;
+
+ALTER TABLE accounts
+ADD CONSTRAINT fk_branch
+FOREIGN KEY (branch_id)
+REFERENCES branches(branch_id);
+
